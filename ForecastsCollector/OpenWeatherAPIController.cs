c0faff845f2a5795244fd127e8189dbe748a93e5
Nick Malygin <ndmalygin@gmail.com>
2023-02-03 12:30:51 +0300

    Cleanup

--- ForecastsCollector/OpenWeatherAPIController.cs
+++ ForecastsCollector/OpenWeatherAPIController.cs
@@ -1,49 +1,50 @@
 ﻿using System.Diagnostics;
-namespace ForecastsCollector;
-
-using ForecastsCollector.Enums;
-using ForecastsCollector.JsonEntities;
-using Newtonsoft.Json;
-
-public class OpenWeatherAPIController
+namespace ForecastsCollector
 {
-    private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
-    private readonly HttpClient _client = new();
+    using ForecastsCollector.Enums;
+    using ForecastsCollector.JsonEntities;
+    using Newtonsoft.Json;
 
-    private readonly string _apiKey;
-    public OpenWeatherAPIController(string apiKey)
+    public class OpenWeatherAPIController
     {
-        _apiKey = apiKey;
-    }
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        private readonly HttpClient _client = new();
 
-    public async Task<string> GetWeather(string cityName)
-    {
-        try
+        private readonly string _apiKey;
+        public OpenWeatherAPIController(string apiKey)
         {
-            var geoUri = GetGeocodeCallUri(cityName, string.Empty, string.Empty, 1);
-            string responseBody = await _client.GetStringAsync(geoUri);
-            var geoCodes = JsonConvert.DeserializeObject<List<GeoCodes>>(responseBody);
-            Debug.Assert(geoCodes?.Count == 1);
-
-            var weatherUri = GetWeatherCallUri(geoCodes[0].lat, geoCodes[0].lon, ForecastUnits.metric);
-            responseBody = await _client.GetStringAsync(weatherUri);
-
-            return await Task.FromResult(responseBody);
+            _apiKey = apiKey;
         }
-        catch (Exception e)
+
+        public async Task<string> GetWeather(string cityName)
         {
-            Logger.Error(e);
-            return await Task.FromResult(string.Empty);
+            try
+            {
+                var geoUri = GetGeocodeCallUri(cityName, string.Empty, string.Empty, 1);
+                string responseBody = await _client.GetStringAsync(geoUri);
+                var geoCodes = JsonConvert.DeserializeObject<List<GeoCodes>>(responseBody);
+                Debug.Assert(geoCodes?.Count == 1);
+
+                var weatherUri = GetWeatherCallUri(geoCodes[0].lat, geoCodes[0].lon, ForecastUnits.metric);
+                responseBody = await _client.GetStringAsync(weatherUri);
+
+                return await Task.FromResult(responseBody);
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e);
+                return await Task.FromResult(string.Empty);
+            }
         }
-    }
 
-    private string GetGeocodeCallUri(string cityName, string stateCode, string countryCode, int limit)
-    {
-        return $"http://api.openweathermap.org/geo/1.0/direct?q={cityName},{stateCode},{countryCode}&limit={limit}&appid={_apiKey}";
-    }
+        private string GetGeocodeCallUri(string cityName, string stateCode, string countryCode, int limit)
+        {
+            return $"http://api.openweathermap.org/geo/1.0/direct?q={cityName},{stateCode},{countryCode}&limit={limit}&appid={_apiKey}";
+        }
 
-    private string GetWeatherCallUri(double lat, double lon, ForecastUnits units)
-    {
-        return $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units={Enum.GetName(units)}&appid={_apiKey}";
+        private string GetWeatherCallUri(double lat, double lon, ForecastUnits units)
+        {
+            return $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units={Enum.GetName(units)}&appid={_apiKey}";
+        }
     }
 }
