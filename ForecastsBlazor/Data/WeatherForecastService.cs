cc178f5d31a5d641aa67171b0e57f1ee99fbec67
Nick Malygin <ndmalygin@gmail.com>
2023-02-08 20:21:38 +0300

    Update WeatherForecastService.cs and ForecastsBlazor.csproj

--- ForecastsBlazor/Data/WeatherForecastService.cs
+++ ForecastsBlazor/Data/WeatherForecastService.cs
@@ -1,17 +1,28 @@
+using Config.Net;
 using ForecastsCommon.JsonEntities;
 using ForecastsCollector;
 namespace ForecastsBlazor.Data;
 public class WeatherForecastService
 {
+    private string _mongoUri;
+    public WeatherForecastService()
+    {
+        var settings = new ConfigurationBuilder<ISettings>()
+           .UseIniFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "forecaster.ini"))
+           .Build();
+
+        _mongoUri = settings.mongodb_connection;
+    }
+
     public Task<Weather[]> GetForecastAsync(string city)
     {
-        var mongoDBDispatcher = new MongoDBDispatcher("mongodb://localhost:27017");
-         return Task.FromResult(mongoDBDispatcher.GetWeathers(city).ToArray());
+        var mongoDBDispatcher = new MongoDBDispatcher(_mongoUri);
+        return Task.FromResult(mongoDBDispatcher.GetWeathers(city).ToArray());
     }
 
     public Task<string[]> GetCitiesNamesAsync()
     {
-        var mongoDBDispatcher = new MongoDBDispatcher("mongodb://localhost:27017");
-         return Task.FromResult(mongoDBDispatcher.GetCities().ToArray());
+        var mongoDBDispatcher = new MongoDBDispatcher(_mongoUri);
+        return Task.FromResult(mongoDBDispatcher.GetCities().ToArray());
     }
 }
