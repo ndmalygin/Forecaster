2cb69939d53bd489b300ecf5668448492cc5a292
ndmalygin <ndmalygin@gmail.com>
2026-08-21 13:40:07 +0300

    Add Open Telemetry

--- ForecastsWebApp/Program.cs
+++ ForecastsWebApp/Program.cs
@@ -1,6 +1,37 @@
 using ForecastsWebApp.Data;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var serviceName = "ForecastsWebApp";
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(serviceName))
+    .WithTracing(tracing => tracing
+        .AddAspNetCoreInstrumentation() // Trace HTTP
+        .AddHttpClientInstrumentation() // Trace OpenWeather API
+        .AddOtlpExporter(options => 
+        {
+            // Jaeger, Grafana Tempo or OTel Collector
+            options.Endpoint = new Uri("http://localhost:4317"); 
+        }))
+    .WithMetrics(metrics => metrics
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddRuntimeInstrumentation() // GC, Memory,CPU metrics
+        .AddOtlpExporter());
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName));
+    
+    options.IncludeFormattedMessage = true;
+    options.IncludeScopes = true;
+    
+    options.AddOtlpExporter(); 
+});
 
 // Add services to the container.
 builder.Services.AddRazorPages();
